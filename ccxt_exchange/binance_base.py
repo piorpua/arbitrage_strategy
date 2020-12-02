59c47c200767e92caae534a8d3240156320ca3bc
piorpua <zhangyaxiong@bituniverse.org>
2020-12-02 17:40:23 +0800

    [ccxt_exchange] binance_base: fetch_funding_rate_history

--- ccxt_exchange/binance_base.py
+++ ccxt_exchange/binance_base.py
@@ -45,3 +45,43 @@ class binance_base (binance):
                 'lastFundingRate': self.safe_float(funding_rate_info, 'lastFundingRate')
             })
         return result
+
+    async def fetch_funding_rate_history(self, symbol, since=None, limit=None, params=None):
+        params = params or {}
+
+        await self.load_markets()
+        market = self.market(symbol)
+        request = {
+            'symbol': market['id'],
+        }
+
+        if since is not None:
+            request['startTime'] = since
+
+        if limit is not None:
+            request['limit'] = min(limit, 1000)
+
+        defaultType = self.safe_string_2(self.options, 'fetchBidsAsks', 'defaultType', 'spot')
+        type = self.safe_string(params, 'type', defaultType)
+
+        query = self.omit(params, 'type')
+        if type == 'future':
+            method = 'fapiPublicGetFundingRate'
+        elif type == 'delivery':
+            method = 'dapiPublicGetFundingRate'
+        else:
+            raise ExchangeError(self.id + " does not support '" + type + "' type, set exchange.options['defaultType'] to 'spot', 'margin', 'delivery' or 'future'")
+        response = await getattr(self, method)(self.extend(request, query))
+        # {
+        #     "symbol": "BTCUSDT",
+        #     "fundingTime": 1606896000011,
+        #     "fundingRate": "0.00010000"
+        # }
+        result = []
+        for funding_rate_info in response:
+            result.append({
+                'info': funding_rate_info,
+                'fundingTime': self.safe_integer(funding_rate_info, 'fundingTime'),
+                'fundingRate': self.safe_float(funding_rate_info, 'fundingRate')
+            })
+        return result
